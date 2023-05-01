1db180b8641f63787156d897f7c47260d4742b3a
waridhasan195 <waridhasan195@gmail.com>
2023-05-01 23:54:02 +0600

    Nope

---
+++ MyWebApp/src/main/java/com/mycompany/MyWebApp/MainController.java
@@ -0,0 +1,13 @@
+package com.mycompany.MyWebApp;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("")
+    public String showHomePage(){
+        System.out.println("SomeThing");
+        return "index";
+    }
+}
